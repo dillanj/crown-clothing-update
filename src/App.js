e6474fb3a0a6d57d3da6a5da56d15b5790678e34
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-03-24 22:08:27 -0600

    lesson 72 finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-
-
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
+
   return (
-    <div className="categories-container">
-      <div className="category-container">
-        <img></img>
-        div.category-body-container 
-      </div>
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <div>{category.title}</div>
+        // <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
