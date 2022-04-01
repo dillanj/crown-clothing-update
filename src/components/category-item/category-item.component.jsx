98ca078dfb6017e4c2171e512f3455ab66db0043
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-03-31 22:41:21 -0600

    lesson 76 done

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -3,12 +3,12 @@ import './category-item.styles.scss';
 const CategoryItem = ({category}) => {
   const { imageUrl, title } = category;
   return(
-    <div class="category-container">
-      <div class="background-image" style={{
+    <div className="category-container">
+      <div className="background-image" style={{
         backgroundImage: `url(${imageUrl})`
       }}>
       </div>
-      <div class="category-body-container">
+      <div className="category-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
