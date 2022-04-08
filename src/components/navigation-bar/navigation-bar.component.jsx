2f8ca3fb82e4ccd3a25d632d888d60969caf8bce
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-04-07 22:28:25 -0600

    lesson 83 done - the last commit was actually 82

--- src/components/navigation-bar/navigation-bar.component.jsx
+++ src/components/navigation-bar/navigation-bar.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
 import './navigation-bar.styles.scss'
 
 const NavigationBar = () => {
@@ -9,7 +11,7 @@ const NavigationBar = () => {
       <nav className='navigation'>
 
         <Link className='logo-container' to='/'>
-          <div>Logo</div>
+          <CrownLogo className='logo'/>
         </Link>
 
         <div className="nav-links-container">
