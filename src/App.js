74b9f8f53ce6039b938222035d9e362658a1347a
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-04-26 21:54:58 -0600

    lesson 112 finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 
+
 import Home from "./routes/home/home.component";
 import NavigationBar from './components/navigation-bar/navigation-bar.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
 
@@ -10,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<NavigationBar />}>
         <Route index element={<Home />}></Route>
+        <Route path='shop' element={<Shop />} />
         <Route path='/auth' element={<Authentication />}></Route>
       </Route>
     </Routes>
