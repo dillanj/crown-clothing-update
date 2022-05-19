8e0fee067709eac14a9cc2c32c4ee171cb6aeee3
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-05-18 22:41:24 -0600

    lesson 134 done

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,14 @@
 import ProductCard from "../../components/product-card/product-card.component";
+import { Link } from "react-router-dom";
 import "./category-preview.styles.scss";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link className="title" to={title}>
+          <span>{title.toUpperCase()}</span>
+        </Link>
       </h2>
       <div className="preview">
         {products
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../Category/Category.component";
 
 const Shop = () => {
   return (
     <Routes>
-      <Route index element={<CategoriesPreview />}></Route>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
