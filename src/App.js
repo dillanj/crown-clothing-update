53c7de75b2f0da5bb989153781931ad002b77b54
Dillan Johnson <dillanjohnson@Dillans-MacBook-Pro.local>
2022-07-29 22:05:37 -0600

    lesson 235 finished, section 27 finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
-// import {
-//   createUserDocumentFromAuth,
-//   getCurrentUser,
-//   onAuthStateChangedListener,
-// } from "./utils/firebase/firebase.utils";
-
 import { checkUserSession } from "./store/user/user.action";
 
-import Home from "./routes/home/home.component";
-import NavigationBar from "./components/navigation-bar/navigation-bar.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
+// import Home from "./routes/home/home.component";
+// import NavigationBar from "./components/navigation-bar/navigation-bar.component";
+// import Authentication from "./routes/authentication/authentication.component";
+// import Shop from "./routes/shop/shop.component";
+// import Checkout from "./routes/checkout/checkout.component";
+import Spinner from "./components/spinner/spinner.component";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
+const NavigationBar = lazy(() =>
+  import("./components/navigation-bar/navigation-bar.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -22,27 +27,17 @@ const App = () => {
     dispatch(checkUserSession());
   });
 
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChangedListener((userAuth) => {
-  //     if (userAuth) {
-  //       createUserDocumentFromAuth(userAuth);
-  //     }
-  //     // dispatch({ type: "SET_CURRENT_USER", payload: userAuth });
-  //     dispatch(setCurrentUser(userAuth));
-  //   });
-
-  //   return unsubscribe;
-  // }, [dispatch]);
-
   return (
-    <Routes>
-      <Route path="/" element={<NavigationBar />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<NavigationBar />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
